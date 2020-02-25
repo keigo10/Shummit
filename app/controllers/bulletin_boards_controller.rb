1c8ad5d86205fae5924a0e16ed14980c2fb82154
keigo10 <keigoiphone0325@gmail.com>
2020-02-25 04:34:54 +0000

    detail

--- app/controllers/bulletin_boards_controller.rb
+++ app/controllers/bulletin_boards_controller.rb
@@ -1,18 +1,24 @@
 class BulletinBoardsController < ApplicationController
 
 	def destroy
-		event = Event.find(params[:event_id])
-	    comment = current_user.bulletin_boards.find(params[:id])
-	    comment.destroy
-	    redirect_back(fallback_location: root_path)
+	    @bulletin_board = current_user.bulletin_boards.find_by(id: params[:id])
+	    event = @bulletin_board.event
+	    @bulletin_board.destroy
+	    respond_to do |format|
+	    	format.js {render:"destroy",:locals => {@bulletin_board =>@bulletin_board}}
+	    end
+	    #@redirect_back(fallback_location: root_path)
     end
 
 	def create
 	    event = Event.find(params[:event_id])
-	    comment = current_user.bulletin_boards.new(bulletin_board_params)
-	    comment.event_id = event.id
-	    comment.save
-	    redirect_back(fallback_location: root_path)
+	    @bulletin_board = current_user.bulletin_boards.new(bulletin_board_params)
+	    @bulletin_board.event_id = event.id
+	    @bulletin_board.save
+	    respond_to do |format|
+	    	format.js {render:"create",:locals => {@bulletin_board =>@bulletin_board,@event => event}}
+	    end
+	    # redirect_back(fallback_location: root_path)
     end
 
     private
