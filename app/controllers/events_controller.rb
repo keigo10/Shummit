641fa6c1f32dcd925e95e9881ece30d745c5ade8
keigo10 <keigoiphone0325@gmail.com>
2020-03-28 11:24:07 +0000

    my-page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
 
+  before_action :authenticate_user!
+
   # GET /events
   # GET /events.json
   def index
--- spec/controller/users_spec.rb
+++ spec/controller/users_spec.rb
@@ -4,21 +4,21 @@ require 'rails_helper'
 
   describe 'ユーザー認証のテスト' do
     describe 'ユーザー新規登録' do
-      before do
-        visit new_user_registration_path
-      end
       context '新規登録画面に遷移' do
         it '新規登録に成功する' do
+          visit new_user_registration_path
           fill_in 'user[family_name]', with: Faker::Internet.username(specifier: 5)
           fill_in 'user[first_name]', with: Faker::Internet.username(specifier: 5)
           fill_in 'user[email]', with: Faker::Internet.email
           fill_in 'user[password]', with: 'password'
           fill_in 'user[password_confirmation]', with: 'password'
-          
+
           click_button 'Sign up'
         end
         it '新規登録に失敗する' do
-          fill_in 'user[name]', with: ''
+          visit new_user_registration_path
+          fill_in 'user[family_name]', with: ''
+          fill_in 'user[family_name]', with: ''
           fill_in 'user[email]', with: ''
           fill_in 'user[password]', with: ''
           fill_in 'user[password_confirmation]', with: ''
